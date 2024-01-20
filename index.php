f0110366cd25abde22d8d6dac026e859647a1928
Janos <janostech@gmail.com>
2024-01-20 13:49:57 +0000

    Hatodik

--- index.php
+++ index.php
@@ -24,9 +24,7 @@ else
 <body>
     <section class="congratulations">
         <h1>Congrats yeah!</h1>
-        <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
-        <p>You are running PHP version <?= phpversion() ?></p>
-        <p>This environment is launched with Elastic Beanstalk PHP Platform</p>
+        <p>CI/CD deployed code via Elastic beanstalk</p>
     </section>
 
     <section class="instructions">
